1658d5389a988f5b09d18113ffe69ba0fda94c94
Hossein Zolfi <hossein.zolfi@gmail.com>
2021-04-08 10:42:08 +0430

    Use shorter codes

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -8,10 +8,13 @@ import (
 var counter int64
 var generator interface{ Generate() int64 }
 
+// First 20bits are redundant(in most cases), we can use shorter code
+var Mask = int64(0x00_00_0F_FF_FF_FF_FF_FF)
+
 func init() {
 	generator = snowflake.MustNew()
 }
 
 func New() string {
-	return encoding.Encode(generator.Generate())
+	return encoding.Encode(generator.Generate() & Mask)
 }
